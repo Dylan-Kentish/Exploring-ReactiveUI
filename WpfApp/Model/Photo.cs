010e4a60f65cede0590829b7a9f81b18158043e0
Dylan Kentish <dylankentish@live.co.uk>
2022-08-03 12:39:18 +0100

    Added Photo and PhotoService

---
+++ WpfApp/Model/Photo.cs
@@ -0,0 +1,18 @@
+﻿namespace WpfApp.Model
+{
+    public class Photo
+    {
+        public Photo(int id, string title, string url, string thumbnailUrl)
+        {
+            Id = id;
+            Title = title;
+            Url = url;
+            ThumbnailUrl = thumbnailUrl;
+        }
+
+        public int Id { get; }
+        public string Title { get; }
+        public string Url { get; }
+        public string ThumbnailUrl { get; }
+    }
+}
---
+++ WpfApp/Services/IPhotoService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WpfApp.Model;
+
+namespace WpfApp.Services;
+
+public interface IPhotoService
+{
+    Task<IEnumerable<Photo>> GetAlbumPhotos(int albumId);
+}
