7ea99afad69f67e95ceeb27e97978d37bd4c7a60
Dylan Kentish <dylankentish@live.co.uk>
2022-08-03 12:41:47 +0100

    Create PhotoVM.cs
    Create ImageSource from the Url

---
+++ WpfApp/ViewModels/PhotoVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft;
+using WpfApp.Model;
+
+namespace WpfApp.ViewModels
+{
+    public class PhotoVM
+    {
+        public PhotoVM(Photo model)
+        {
+            Requires.NotNull(model, nameof(model));
+
+            Photo = new BitmapImage(
+                new Uri(model.Url));
+            Thumbnail = new BitmapImage(
+                new Uri(model.ThumbnailUrl));
+        }
+
+        public ImageSource Photo { get; }
+        public ImageSource Thumbnail { get; }
+    }
+}
