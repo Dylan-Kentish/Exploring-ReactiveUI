f62f300db9c7a7fd3775f535930f6c44f620a601
Dylan Kentish <dylankentish@live.co.uk>
2022-08-03 12:40:18 +0100

    Implemented IPhotoService in DataService

--- WpfApp/Services/DataService.cs
+++ WpfApp/Services/DataService.cs
@@ -5,16 +5,19 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using WpfApp.Model;
 
 
 namespace WpfApp.Services;
 
-public class DataService : IAlbumService
+public class DataService : IAlbumService, IPhotoService
 {
     private const string _uriString = "https://jsonplaceholder.typicode.com/";
     private const string _albumsPath = _uriString + "albums";
+    private const string _photosPath = _uriString + "photos";
 
 
     private readonly HttpClient _client;
@@ -39,6 +42,28 @@ public class DataService : IAlbumService
         }
 
         return albums ?? Enumerable.Empty<Album>();
+    }
+
+    public async Task<IEnumerable<Photo>> GetAlbumPhotos(int albumId)
+    {
+        var photosUrl = AddQuery(_photosPath, nameof(albumId), albumId.ToString());
+        IEnumerable<Photo>? photos = null;
+        HttpResponseMessage response = await _client.GetAsync(photosUrl);
+        if (response.IsSuccessStatusCode)
+        {
+            photos = await response.Content.ReadFromJsonAsync<IEnumerable<Photo>>();
+        }
+
+        return photos ?? Enumerable.Empty<Photo>();
+    }
+
+    private string AddQuery(string url, string queryProperty, string queryValue)
+    {
+        if (url.Contains('?'))
+        {
+            return url + $"&{queryProperty}={queryValue}";
+        }
+        return url + $"?{queryProperty}={queryValue}";
     }
 }
 
