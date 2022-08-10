ce884941eea7872ea07555f0923a96d7cc1fab50
Dylan Kentish <dylankentish@live.co.uk>
2022-08-10 11:15:41 +0100

    Added UserVM

---
+++ WpfApp/ViewModels/UserVM.cs
@@ -0,0 +1,15 @@
+﻿using WpfApp.Model;
+
+namespace WpfApp.ViewModels;
+
+public class UserVM
+{
+    private readonly User _model;
+
+    public UserVM(User model)
+    {
+        _model = model;
+    }
+
+    public string Username => _model.Username;
+}
