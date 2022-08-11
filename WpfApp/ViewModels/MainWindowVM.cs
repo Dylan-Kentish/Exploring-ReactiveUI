0fe358b5fcb8615d4feed4b08244d536fff5e705
Dylan Kentish <dylankentish@live.co.uk>
2022-08-11 13:30:28 +0100

    Refactored MainWindow to have a simple navigation

--- WpfApp/ViewModels/MainWindowVM.cs
+++ WpfApp/ViewModels/MainWindowVM.cs
@@ -1,29 +1,50 @@
 ﻿using System;
-using System.Reactive.Disposables;
+using System.Windows.Input;
+using ModernWpf.Controls;
 using ReactiveUI;
 using static Microsoft.Requires;
 
 namespace WpfApp.ViewModels
 {
-    public sealed class MainWindowVM : ReactiveObject, IDisposable
+    public sealed class MainWindowVM : ReactiveObject
     {
-        private readonly CompositeDisposable _disposable = new();
+        private Type? _currentPage;
 
-        public MainWindowVM(ChangeThemeVM changeTheme, LoginVM loginVM)
+        public MainWindowVM(ChangeThemeVM changeTheme)
         {
             ChangeThemeVM = NotNull(changeTheme, nameof(changeTheme));
-            LoginVM = NotNull(loginVM, nameof(loginVM));
 
-            _disposable.Add(loginVM);
+            ViewSelected = ReactiveCommand.Create<NavigationViewItem>(ViewSelectedInternal);
+
+            NavigateTo("HomePage");
+        }
+
+        public ICommand ViewSelected { get; }
+
+        public ChangeThemeVM ChangeThemeVM { get; }
+
+        public Type? CurrentPage
+        {
+            get => _currentPage;
+            set => this.RaiseAndSetIfChanged(ref _currentPage, value);
+        }
+
+        private void ViewSelectedInternal(NavigationViewItem obj)
+        {
+            if (obj.Tag is string page)
+            {
+                NavigateTo(page);
+            }
         }
 
 
-        public ChangeThemeVM ChangeThemeVM { get; }
-        public LoginVM LoginVM { get; }
-
-        public void Dispose()
+        private void NavigateTo(string page)
         {
-            _disposable.Dispose();
+            // Really don't like this.
+            // This could be improved by binding the NavigationView.MenuItems
+            // to a collection maintained in this class.
+            var typeString = "WpfApp.Views.Pages." + page;
+            CurrentPage = Type.GetType(typeString);
         }
     }
 }
