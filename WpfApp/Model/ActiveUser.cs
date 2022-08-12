a7b86a363483965b49605113ee6ea5d9d9a28558
Dylan Kentish <dylankentish@live.co.uk>
2022-08-12 11:58:31 +0100

    Added ActiveUser
    Will be used to track the logged in user

---
+++ WpfApp/Model/ActiveUser.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+
+namespace WpfApp.Model
+{
+    public class ActiveUser : ReactiveObject
+    {
+        private User? _user;
+
+        public User? User
+        {
+            get => _user;
+            set => this.RaiseAndSetIfChanged(ref _user, value);
+        }
+    }
+}
