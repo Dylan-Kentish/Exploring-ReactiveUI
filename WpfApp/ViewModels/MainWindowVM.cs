3ebc2a6cc6cdcdc3375566c09b187492459ad7d6
Dylan Kentish <dylankentish@live.co.uk>
2022-08-12 12:01:52 +0100

    Updated MainWindow
    Account details are only visible if you are logged in

--- WpfApp/ViewModels/MainWindowVM.cs
+++ WpfApp/ViewModels/MainWindowVM.cs
@@ -1,22 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using ModernWpf.Controls;
 using ReactiveUI;
+using WpfApp.Model;
 using static Microsoft.Requires;
 
 namespace WpfApp.ViewModels
 {
-    public sealed class MainWindowVM : ReactiveObject
+    public sealed class MainWindowVM : ReactiveObject, IDisposable
     {
         private Type? _currentPage;
+        private CompositeDisposable _disposable;
+        private readonly ObservableAsPropertyHelper<bool> _userLoggedIn;
 
-        public MainWindowVM(ChangeThemeVM changeTheme)
+
+        public MainWindowVM(ChangeThemeVM changeTheme, IObservable<User?> currentUser)
         {
+            _disposable = new CompositeDisposable();
+
             ChangeThemeVM = NotNull(changeTheme, nameof(changeTheme));
 
             ViewSelected = ReactiveCommand.Create<NavigationViewItem>(ViewSelectedInternal);
 
             NavigateTo("HomePage");
+
+            currentUser
+                .Select(user => user != null)
+                .ToProperty(this, x => x.UserLoggedIn, out _userLoggedIn)
+                .DisposeWith(_disposable);
         }
 
         public ICommand ViewSelected { get; }
@@ -29,6 +44,8 @@ namespace WpfApp.ViewModels
             set => this.RaiseAndSetIfChanged(ref _currentPage, value);
         }
 
+        public bool UserLoggedIn => _userLoggedIn.Value;
+
         private void ViewSelectedInternal(NavigationViewItem obj)
         {
             if (obj.Tag is string page)
@@ -37,7 +54,6 @@ namespace WpfApp.ViewModels
             }
         }
 
-
         private void NavigateTo(string page)
         {
             // Really don't like this.
@@ -45,6 +61,11 @@ namespace WpfApp.ViewModels
             // to a collection maintained in this class.
             var typeString = "WpfApp.Views.Pages." + page;
             CurrentPage = Type.GetType(typeString);
+        }
+
+        public void Dispose()
+        {
+            _disposable.Dispose();
         }
     }
 }
