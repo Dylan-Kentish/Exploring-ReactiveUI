abf22a68f988b5dc56681e69a73a9c4c9cb3b5fa
Kent <dylankentish@live.co.uk>
2022-08-14 19:42:47 +0100

    Update AlbumVM.cs
    The photos need to be retrieved from the album

--- WpfApp/ViewModels/AlbumVM.cs
+++ WpfApp/ViewModels/AlbumVM.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft;
+﻿using Microsoft;
 using WpfApp.Model;
 
 namespace WpfApp.ViewModels
@@ -8,20 +6,14 @@ namespace WpfApp.ViewModels
     public class AlbumVM
     {
         private readonly Album _model;
-        private readonly IEnumerable<Photo> _photos;
 
-        public AlbumVM(Album model, IEnumerable<Photo> photos)
+        public AlbumVM(Album model)
         {
             Requires.NotNull(model, nameof(model));
-            Requires.NotNull(photos, nameof(photos));
-            Requires.Argument(photos.Any(), nameof(photos), "photos is empty");
 
             _model = model;
-            _photos = photos;
-            Thumbnail = new PhotoVM(_photos.First());
         }
 
         public string Title => _model.Title;
-        public PhotoVM Thumbnail { get; } 
     }
 }
