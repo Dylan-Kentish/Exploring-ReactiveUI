57f6f4626cf1d70e3550f28ddac5acc1f180378e
Kent <dylankentish@live.co.uk>
2022-08-14 16:15:59 +0100

    Create AccountVM.cs

---
+++ WpfApp/ViewModels/AccountVM.cs
@@ -0,0 +1,25 @@
+﻿using ReactiveUI;
+using System;
+using System.Reactive.Disposables;
+using WpfApp.Model;
+
+namespace WpfApp.ViewModels
+{
+    public sealed class AccountVM : ReactiveObject, IDisposable
+    {
+        private readonly CompositeDisposable _disposables;
+        private readonly ObservableAsPropertyHelper<User> _user;
+        public AccountVM(IObservable<User> user)
+        {
+            _disposables = new CompositeDisposable();
+            user.ToProperty(this, x => x.User, out _user);
+        }
+
+        public User User => _user.Value;
+
+        public void Dispose()
+        {
+            _disposables.Dispose();
+        }
+    }
+}
