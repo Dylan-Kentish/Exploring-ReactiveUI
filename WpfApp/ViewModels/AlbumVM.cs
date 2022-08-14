c148e76104ae852992c98e85336ad19cd3b75be8
Kent <dylankentish@live.co.uk>
2022-08-14 23:46:20 +0100

    Update AlbumVM.cs
    Navigates to Album page OnClick
    Gets first Photo for thumbnail

--- WpfApp/ViewModels/AlbumVM.cs
+++ WpfApp/ViewModels/AlbumVM.cs
@@ -1,19 +1,61 @@
-﻿using Microsoft;
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Windows.Input;
 using WpfApp.Model;
+using WpfApp.Services;
+using static Microsoft.Requires;
 
 namespace WpfApp.ViewModels
 {
-    public class AlbumVM
+    public sealed class AlbumVM : ReactiveObject, IDisposable
     {
+        private readonly CompositeDisposable _disposables;
         private readonly Album _model;
+        private readonly INavigationService _navigationService;
+        private readonly ObservableAsPropertyHelper<PhotoVM> _thumbnail;
 
-        public AlbumVM(Album model)
+        public AlbumVM(
+            INavigationService navigationService,
+            Album model)
         {
-            Requires.NotNull(model, nameof(model));
+            _disposables = new CompositeDisposable();
+            _navigationService = NotNull(navigationService, nameof(navigationService));
+            _model = NotNull(model, nameof(model));
 
-            _model = model;
+            Observable.StartAsync(model.GetPhotos)
+                .Select(photos => photos.FirstOrDefault())
+                .WhereNotNull()
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Select(photo => new PhotoVM(photo))
+                .ToProperty(this, x => x.Thumbnail, out _thumbnail)
+                .DisposeWith(_disposables);
+
+            OnClick = ReactiveCommand.Create(OnClickInternal);
         }
 
         public string Title => _model.Title;
+
+        public PhotoVM Thumbnail => _thumbnail.Value;
+
+        public ICommand OnClick { get; }
+
+        private void OnClickInternal()
+        {
+            var parameters = new Dictionary<string, object>()
+            {
+                { nameof(Album), _model }
+            };
+
+            _navigationService.NavigateTo(NavigationService.Album, parameters);
+        }
+
+        public void Dispose()
+        {
+            _disposables.Dispose();
+        }
     }
 }
