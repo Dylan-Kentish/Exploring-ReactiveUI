157e2012702f73af5520d87de8462cf2cf42dae4
Kent <dylankentish@live.co.uk>
2022-08-14 23:48:44 +0100

    Added AlbumPage
    GetsPhotos when navigated to
    Can be reused for same album
    Supports searching

---
+++ WpfApp/ViewModels/PhotosVM.cs
@@ -0,0 +1,83 @@
+﻿using DynamicData;
+using Prism.Regions;
+using ReactiveUI;
+using System;
+using System.Collections.ObjectModel;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using WpfApp.Model;
+
+namespace WpfApp.ViewModels
+{
+    internal class PhotosVM : ReactiveObject, IDisposable, INavigationAware
+    {
+        private readonly CompositeDisposable _disposables;
+        private Album _model;
+        private ReadOnlyObservableCollection<PhotoVM>? _photos;
+        private string? _photoSearch;
+
+        public PhotosVM()
+        {
+            _disposables = new CompositeDisposable();
+        }
+
+        public ReadOnlyObservableCollection<PhotoVM>? Photos => _photos;
+
+        public string? PhotoSearch
+        {
+            get => _photoSearch;
+            set => this.RaiseAndSetIfChanged(ref _photoSearch, value);
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return navigationContext.Parameters["Album"] is Album model &&
+                _model?.Id == model.Id;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            if (_model is null && navigationContext.Parameters["Album"] is Album model)
+            {
+                _model = model;
+
+                Observable.StartAsync(model.GetPhotos)
+                    .Subscribe(photos =>
+                    {
+                        var cache = new SourceCache<Photo, int>(photo => photo.Id);
+                        cache.AddOrUpdate(photos);
+
+                        var albumSearchFilter = this.WhenAnyValue(x => x.PhotoSearch)
+                            .Throttle(TimeSpan.FromSeconds(0.5), RxApp.TaskpoolScheduler)
+                            .Select(query => query?.Trim())
+                            .DistinctUntilChanged()
+                            .ObserveOn(RxApp.MainThreadScheduler)
+                            .Select(MakePhotoFilter);
+
+                        cache
+                            .Connect()
+                            .ObserveOn(RxApp.MainThreadScheduler)
+                            .Transform(x => new PhotoVM(x))
+                            .Bind(out _photos)
+                            .Subscribe(a => this.RaisePropertyChanged(nameof(Photos)))
+                            .DisposeWith(_disposables);
+                    })
+                    .DisposeWith(_disposables);
+            }
+        }
+
+        private static Func<Photo, bool> MakePhotoFilter(string? filter)
+        {
+            return album => string.IsNullOrWhiteSpace(filter) || album.Title.Contains(filter, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public void Dispose()
+        {
+            _disposables.Dispose();
+        }
+    }
+}
---
+++ WpfApp/Views/Pages/AlbumPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for AlbumPage.xaml
+    /// </summary>
+    public partial class AlbumPage : Grid
+    {
+        public AlbumPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
