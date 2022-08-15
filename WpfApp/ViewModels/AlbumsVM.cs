657aa5614096a4793f25a11f06ebb9a63e7f9b7d
Dylan Kentish <dylankentish@live.co.uk>
2022-08-15 11:01:05 +0100

    Update AlbumsVM.cs
    Now INavigationAware
    
    The user is passed as a parameter and the view can be reused for the same user

--- WpfApp/ViewModels/AlbumsVM.cs
+++ WpfApp/ViewModels/AlbumsVM.cs
@@ -5,48 +5,25 @@ using System.Collections.ObjectModel;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using WpfApp.Model;
-using System.Linq;
 using DynamicData;
+using Prism.Regions;
 using WpfApp.Services;
 
 namespace WpfApp.ViewModels
 {
-    public sealed class AlbumsVM : ReactiveValidationObject, IDisposable
+    public sealed class AlbumsVM : ReactiveValidationObject, IDisposable, INavigationAware
     {
         private readonly CompositeDisposable _disposables;
         private readonly INavigationService _navigationService;
         private ReadOnlyObservableCollection<AlbumVM>? _albums;
         private string? _albumSearch;
+        private User? _user;
 
         public AlbumsVM(
-            INavigationService navigationService,
-            IObservable<User?> user)
+            INavigationService navigationService)
         {
             _disposables = new CompositeDisposable();
             _navigationService = navigationService;
-
-            user.WhereNotNull()
-                .Subscribe(async user =>
-            {
-                var albums = new SourceCache<Album, int>(a => a.Id);
-                albums.AddOrUpdate(await user.GetAlbums());
-
-                var albumSearchFilter = this.WhenAnyValue(x => x.AlbumSearch)
-                    .Throttle(TimeSpan.FromSeconds(0.5), RxApp.TaskpoolScheduler)
-                    .Select(query => query?.Trim())
-                    .DistinctUntilChanged()
-                    .ObserveOn(RxApp.MainThreadScheduler)
-                    .Select(MakeAlbumFilter);
-
-                albums
-                    .Connect()
-                    .Filter(albumSearchFilter)
-                    .Transform(x => new AlbumVM(_navigationService, x))
-                    .ObserveOn(RxApp.MainThreadScheduler)
-                    .Bind(out _albums)
-                    .Subscribe(a => this.RaisePropertyChanged(nameof(Albums)))
-                    .DisposeWith(_disposables);
-            }).DisposeWith(_disposables);
         }
 
         public ReadOnlyObservableCollection<AlbumVM>? Albums => _albums;
@@ -55,6 +32,50 @@ namespace WpfApp.ViewModels
         {
             get => _albumSearch;
             set => this.RaiseAndSetIfChanged(ref _albumSearch, value);
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            if (_user is not null || navigationContext.Parameters["User"] is not User user)
+            {
+                return;
+            }
+
+            _user = user;
+
+            Observable.StartAsync(user.GetAlbums)
+                .Subscribe(albums =>
+                {
+                    var cache = new SourceCache<Album, int>(a => a.Id);
+                    cache.AddOrUpdate(albums);
+
+                    var albumSearchFilter = this.WhenAnyValue(x => x.AlbumSearch)
+                        .Throttle(TimeSpan.FromSeconds(0.5), RxApp.TaskpoolScheduler)
+                        .Select(query => query?.Trim())
+                        .DistinctUntilChanged()
+                        .ObserveOn(RxApp.MainThreadScheduler)
+                        .Select(MakeAlbumFilter);
+
+                    cache
+                        .Connect()
+                        .Filter(albumSearchFilter)
+                        .Transform(x => new AlbumVM(_navigationService, x))
+                        .ObserveOn(RxApp.MainThreadScheduler)
+                        .Bind(out _albums)
+                        .Subscribe(a => this.RaisePropertyChanged(nameof(Albums)))
+                        .DisposeWith(_disposables);
+
+                }).DisposeWith(_disposables);
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return navigationContext.Parameters["User"] is User user &&
+                   _user?.Id == user.Id;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
         }
 
         private static Func<Album, bool> MakeAlbumFilter(string? filter)
