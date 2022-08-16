0273e309b46ff84598d4f15e0bb219cbd694438d
Dylan Kentish <dylankentish@live.co.uk>
2022-08-16 16:56:43 +0100

    Added Virtualisation to Album Page

--- WpfApp/ViewModels/PhotosVM.cs
+++ WpfApp/ViewModels/PhotosVM.cs
@@ -5,20 +5,76 @@ using System;
 using System.Collections.ObjectModel;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Windows.Input;
 using WpfApp.Model;
 
 namespace WpfApp.ViewModels;
 
 internal class PhotosVM : ReactiveObject, IDisposable, INavigationAware
 {
+    private const int Size = 6;
     private readonly CompositeDisposable _disposables;
+    private IVirtualRequest _virtualRequest;
     private Album? _model;
-    private ReadOnlyObservableCollection<PhotoVM>? _photos;
+    private readonly ReadOnlyObservableCollection<PhotoVM>? _photos;
     private string? _photoSearch;
+    private readonly SourceCache<Photo, int> _cache;
 
     public PhotosVM()
     {
         _disposables = new CompositeDisposable();
+        _virtualRequest = new VirtualRequest(0, Size);
+        _cache = new SourceCache<Photo, int>(photo => photo.Id);
+        _cache.DisposeWith(_disposables);
+
+        var photosSearchFilter = this.WhenAnyValue(x => x.PhotoSearch)
+            .Throttle(TimeSpan.FromSeconds(0.5), RxApp.TaskpoolScheduler)
+            .Select(query => query?.Trim())
+            .DistinctUntilChanged()
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Select(MakePhotoFilter);
+
+        var virtualRequests = this.WhenAnyValue(x => x.VirtualRequest);
+
+        var orderedCache = _cache
+            .Connect()
+            .Filter(photosSearchFilter)
+            .Transform(x => new PhotoVM(x))
+            .SortBy(photo => photo.Title);
+
+        var observableOrderedCache = orderedCache.AsObservableCache();
+
+        observableOrderedCache.DisposeWith(_disposables);
+
+        orderedCache
+            .Virtualise(virtualRequests)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Bind(out _photos)
+            .Subscribe()
+            .DisposeWith(_disposables);
+
+        var canNext = virtualRequests
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .CombineLatest(observableOrderedCache.CountChanged,
+            (r, c) =>
+            {
+                var endIndex = r.StartIndex + r.Size;
+                var can = c - endIndex - 1 > 0;
+
+                if (endIndex > c - 1)
+                {
+                    VirtualRequest = new VirtualRequest(c / Size, Size);
+                }
+
+                return can;
+            });
+
+        var canPrevious = virtualRequests
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Select(request => request.StartIndex != 0);
+
+        Previous = ReactiveCommand.Create(PreviousInternal, canPrevious);
+        Next = ReactiveCommand.Create(NextInternal, canNext);
     }
 
     public ReadOnlyObservableCollection<PhotoVM>? Photos => _photos;
@@ -27,6 +83,16 @@ internal class PhotosVM : ReactiveObject, IDisposable, INavigationAware
     {
         get => _photoSearch;
         set => this.RaiseAndSetIfChanged(ref _photoSearch, value);
+    }
+
+    public ICommand Previous { get; }
+
+    public ICommand Next { get; }
+
+    private IVirtualRequest VirtualRequest
+    {
+        get => _virtualRequest;
+        set => this.RaiseAndSetIfChanged(ref _virtualRequest, value);
     }
 
     public bool IsNavigationTarget(NavigationContext navigationContext)
@@ -48,29 +114,10 @@ internal class PhotosVM : ReactiveObject, IDisposable, INavigationAware
 
         _model = model;
 
-        var cache = new SourceCache<Photo, int>(photo => photo.Id);
-        cache.DisposeWith(_disposables);
-
-        var photosSearchFilter = this.WhenAnyValue(x => x.PhotoSearch)
-            .Throttle(TimeSpan.FromSeconds(0.5), RxApp.TaskpoolScheduler)
-            .Select(query => query?.Trim())
-            .DistinctUntilChanged()
-            .ObserveOn(RxApp.MainThreadScheduler)
-            .Select(MakePhotoFilter);
-
-        cache
-            .Connect()
-            .Filter(photosSearchFilter)
-            .Transform(x => new PhotoVM(x))
-            .ObserveOn(RxApp.MainThreadScheduler)
-            .Bind(out _photos)
-            .Subscribe()
-            .DisposeWith(_disposables);
-        
         Observable.StartAsync(_model.GetPhotos, RxApp.TaskpoolScheduler)
             .Subscribe(photos =>
             {
-                cache.Edit(innerCache =>
+                _cache.Edit(innerCache =>
                 {
                     innerCache.Clear();
                     innerCache.AddOrUpdate(photos);
@@ -81,6 +128,16 @@ internal class PhotosVM : ReactiveObject, IDisposable, INavigationAware
     private static Func<Photo, bool> MakePhotoFilter(string? filter)
     {
         return album => string.IsNullOrWhiteSpace(filter) || album.Title.Contains(filter, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    private void PreviousInternal()
+    {
+        VirtualRequest = new VirtualRequest(VirtualRequest.StartIndex - Size, Size);
+    }
+
+    private void NextInternal()
+    {
+        VirtualRequest = new VirtualRequest(VirtualRequest.StartIndex + Size, Size);
     }
 
     public void Dispose()
