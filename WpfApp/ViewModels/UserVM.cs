a7204d4ef0da43d6bbbeab5c6eafea8fa731802c
Dylan Kentish <dylankentish@live.co.uk>
2022-08-16 13:19:19 +0100

    Exposed additional properties in UserVM

--- WpfApp/ViewModels/UserVM.cs
+++ WpfApp/ViewModels/UserVM.cs
@@ -14,7 +14,19 @@ public class UserVM
 
     public int Id => _model.Id;
 
+    public string Name => _model.Name;
+
     public string Username => _model.Username;
+
+    public string Email => _model.Email;
+
+    public Address Address => _model.Address;
+
+    public string Phone => _model.Phone;
+
+    public string Website => _model.Website;
+
+    public Company Company => _model.Company;
 
     public override string ToString() => Username;
 }
